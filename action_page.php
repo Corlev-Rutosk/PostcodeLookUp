e29df6419a928a273c1ebe896e43c64105def6f0
Louis Cornell <lou.cornell@gmail.com>
2019-07-26 15:20:31 +0100

    php file returns information from postcodes.io for an arbitrary postcode

--- action_page.php
+++ action_page.php
@@ -1,14 +1,30 @@
 <html>
 <body>
 
-
 The postcode is 
+<?php echo $_GET['postcode']; ?> <br> <br>
 
 <?php
 
-echo $_GET['postcode'];
+
+// initialise session
+$curlcon = curl_init();
+
+// set options
+curl_setopt($curlcon,CURLOPT_URL,"http://postcodes.io/postcodes/ng243ng");
+curl_setopt($curlcon,CURLOPT_RETURNTRANSFER,true);
+curl_setopt($curlcon,CURLOPT_HEADER,false); 
+
+$output=curl_exec($curlcon);
+
+curl_close($curlcon);
+
+echo $output;
+
+
 
 ?>
+
 
 
 </body>
