77be3658744bef5f5ded7e1857af9679b56d857a
Louis Cornell <lou.cornell@gmail.com>
2019-07-26 19:11:44 +0100

    curl output is now exploded into an array, allowing access to individual key:value pairs

--- action_page.php
+++ action_page.php
@@ -2,23 +2,45 @@
 <body>
 
 
-<?php $postcode = $_GET['postcode'];
+<?php
 
-// Initialise session
+$postcode = $_GET['postcode'];
+
+// initialise session
 $curlcon = curl_init();
 
-// Set options
+// set options
 curl_setopt($curlcon,CURLOPT_URL,"http://postcodes.io/postcodes/$postcode");
 curl_setopt($curlcon,CURLOPT_RETURNTRANSFER,true);
 curl_setopt($curlcon,CURLOPT_HEADER,false); 
 
 // Fetch data
 $output=curl_exec($curlcon);
-
+    
 // Close session
 curl_close($curlcon);
 
-echo $output;
+   
+$output2 = explode(',',$output);
+
+echo print_r($output2[0]);
+echo '<br>';
+echo print_r($output2[1]);
+echo '<br>';
+echo print_r($output2[2]);
+echo '<br>';
+echo print_r($output2[3]);
+echo '<br>';
+echo print_r($output2[4]);
+echo '<br>';
+echo print_r($output2[5]);
+echo '<br>';
+echo print_r($output2[6]);
+echo '<br>';
+echo print_r($output2[7]);
+echo '<br>';
+echo print_r($output2[8]);
+echo '<br>';
 
 
 ?>
